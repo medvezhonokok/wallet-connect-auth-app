c060b4f52a60370f7cd6a9e6a0941b4b489d7360
IlyaStepanov1104 <ilyahtml@gmai.com>
2025-03-21 01:17:13 +0300

    add repost

--- wagmi-project/src/components/BalancePage.tsx
+++ wagmi-project/src/components/BalancePage.tsx
@@ -4,8 +4,7 @@ import {useRouter} from 'next/navigation';
 const backendApiUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 export const getTokenFromCookies = () => {
-    if (typeof document === 'undefined') throw new Error('Could not find token from cookie');
-    const match = document.cookie.match(new RegExp('(^| )token=([^;]+)'));
+    const match = document?.cookie.match(new RegExp('(^| )token=([^;]+)'));
     return match ? match[2] : null;
 };
 
